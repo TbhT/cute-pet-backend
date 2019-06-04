ae180e09f9faf5ac99fc5c1231d685b3f4e60ad6
TbhT <hustapple132@gmail.com>
2019-06-04 08:53:36 +0800

    去掉本地调试文件

--- config/db-local.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=db_cute_pet',
-    'username' => 'user',
-    'password' => '147896325',
-    'charset' => 'utf8',
-];
