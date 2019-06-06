cec1d8ad4fd55f74004ed365189975d62dd4fac8
TbhT <hustapple132@gmail.com>
2019-06-06 09:47:07 +0800

    增加对json输入数据的是支持

--- config/web.php
+++ config/web.php
@@ -26,7 +26,10 @@ $config = [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => '1ab53b152b4b06986e0726a8f7feaedb',
-            'enableCsrfValidation' => $enableCsrfValidation
+            'enableCsrfValidation' => $enableCsrfValidation,
+            'parsers' => [
+                'application/json' => 'yii\web\JsonParser'
+            ]
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
