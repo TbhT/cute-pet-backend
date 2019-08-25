4a8fb492ba2e8f540d5442a308764723a9192a79
TbhT <hustapple132@gmail.com>
2019-08-25 17:09:58 +0800

    完善接口

--- config/web.php
+++ config/web.php
@@ -24,7 +24,7 @@ $config = [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => '1ab53b152b4b06986e0726a8f7feaedb',
-            'enableCsrfValidation' => false,
+//            'enableCsrfValidation' => false,
             'parsers' => [
                 'application/json' => 'yii\web\JsonParser'
             ]
